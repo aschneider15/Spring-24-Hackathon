06e9d25079749a0886a18799ddb348816e944fde
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2024-02-20 17:02:40 -0600

    Corrected Player Movement

--- 2D Movement Template/2D Player Movement template/Assets/PlayerMove.cs
+++ 2D Movement Template/2D Player Movement template/Assets/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private CharacterController cc; //this helps us with collisions and other physics. 
+    private Rigidbody2D rb; //this helps us with collisions and other physics. 
 
     public float speed;//you can set this value in the editor and edit it quickly.
 
@@ -13,18 +13,19 @@ public class PlayerMove : MonoBehaviour
 
     void Start()
     {
-        //this checks to see it the player already has a character controller, if it does use it, if it doesn't creat a new one.
-        if (this.GetComponent<CharacterController>() == null)
+        //this checks to see it the player already has a rigidBody, if it does use it, if it doesn't creat a new one.
+        if (this.GetComponent<Rigidbody2D>() == null)
         {
-            cc = this.gameObject.AddComponent<CharacterController>();
+            rb = this.gameObject.AddComponent<Rigidbody2D>();
         }
         else
         {
-            cc = this.gameObject.GetComponent<CharacterController>();
+            rb = this.gameObject.GetComponent<Rigidbody2D>();
         }
+        rb.gravityScale = 0;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         //Choose one of the following depending on your game.
 
@@ -36,19 +37,16 @@ public class PlayerMove : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal"); //gets the value from the 'A' and 'D' keys and the right and left arrow keys, returns -1,0,1
 
+        rb.velocity = new Vector2(x*speed, jump());
 
-        Vector2 move = (transform.right * x) * speed;//applies the input value to the x-axis of the player and multiplies it by the movement speed.
-
-        move.y = jump(); //sets the vertaical speed to the return value of the jump function.
-
-        cc.Move(move * Time.deltaTime); //tells the Character Controller to move the correct amount every frame
+       
 
         float jump()
         {
-            if (cc.isGrounded) // checks to see if the player is on the ground
+            if (isGrounded()) // checks to see if the player is on the ground
             {
-          
-                if (Input.GetKey(KeyCode.W)) //checks to see if the user is pressing the jump button 'W'
+                vSpeed = 0;
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) //checks to see if the user is pressing the jump button 'W' or 'Space'
                 {
                     vSpeed = 12; //sets the vertical speed to 12, Change this if you want to jump higher.
 
@@ -59,6 +57,15 @@ public class PlayerMove : MonoBehaviour
             vSpeed -= 29.4f * Time.deltaTime; //simulates gravity, Change this value to increase/decrease the gravity 
 
             return vSpeed;
+         
+
+            bool isGrounded()
+            {
+                int layer_mask = LayerMask.GetMask("Ground");
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, (transform.localScale.y/2) + .1f, layer_mask);
+                return hit;
+                
+            }
         }
     }
 
@@ -67,8 +74,8 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal"); //gets the value from the 'A' and 'D' keys and the right and left arrow keys, returns -1,0,1
         float y = Input.GetAxisRaw("Vertical"); //gets the value from the 'W' and 'S' keys and the up and down arrow keys, returns -1,0,1
 
-        Vector2 move = ((transform.right * x) + (transform.up * y)).normalized * speed; //same as before, normalized makes it so that its a constant speed.
+       rb.velocity = new Vector2(x,y).normalized * speed; //same as before, normalized makes it so that its a constant speed.
 
-        cc.Move(move * Time.deltaTime);//tells the Character Controller to move the correct amount every frame
+        
     }
 }
