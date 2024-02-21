c028cf8e565f0be93341f2b5a50f90a4583d18ef
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2024-02-20 20:48:45 -0600

    Revert "Merge branch 'main' of https://github.com/aschneider15/Spring-24-Hackathon"
    This reverts commit 92944a7649f418bfbf6ac2052eedbf8ca565b672, reversing
    changes made to 84d83a62496173ebcb1fb2c561a6f4fdf6d09582.

--- 2D Movement Template/2D Player Movement template/Assets/Scripts/PlayerMove.cs
+++ 2D Movement Template/2D Player Movement template/Assets/Scripts/PlayerMove.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
     private Rigidbody2D rb; //this helps us with collisions and other physics. 
 
     public float speed;//you can set this value in the editor and edit it quickly.
 
     private float vSpeed = 0;//this is the vertical speed of the player, it simulates gravity.
-    
-    private int spriteVersion = 0;
-    public Sprite[] sprites;
-    private SpriteRenderer sr;
-    public int animSpeed;
 
-    
+
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
-        InvokeRepeating(nameof(changeSprite), this.animSpeed, this.animSpeed);
-
         //this checks to see it the player already has a rigidBody, if it does use it, if it doesn't creat a new one.
         if (this.GetComponent<Rigidbody2D>() == null)
         {
@@ -39,16 +30,16 @@ public class PlayerMove : MonoBehaviour
         //Choose one of the following depending on your game.
 
         //SideScrollMove();
-        TopDownMove();
+        TopDownMove(); 
     }
 
     void SideScrollMove()
     {
         float x = Input.GetAxisRaw("Horizontal"); //gets the value from the 'A' and 'D' keys and the right and left arrow keys, returns -1,0,1
 
-        rb.velocity = new Vector2(x * speed, jump());
+        rb.velocity = new Vector2(x*speed, jump());
 
-
+       
 
         float jump()
         {
@@ -61,19 +52,19 @@ public class PlayerMove : MonoBehaviour
 
                 }
             }
-
+            
 
             vSpeed -= 29.4f * Time.deltaTime; //simulates gravity, Change this value to increase/decrease the gravity 
 
             return vSpeed;
-
+         
 
             bool isGrounded()
             {
                 int layer_mask = LayerMask.GetMask("Ground");
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, (transform.localScale.y / 2) + .1f, layer_mask);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, (transform.localScale.y/2) + .1f, layer_mask);
                 return hit;
-
+                
             }
         }
     }
@@ -83,16 +74,8 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal"); //gets the value from the 'A' and 'D' keys and the right and left arrow keys, returns -1,0,1
         float y = Input.GetAxisRaw("Vertical"); //gets the value from the 'W' and 'S' keys and the up and down arrow keys, returns -1,0,1
 
-        rb.velocity = new Vector2(x, y).normalized * speed; //same as before, normalized makes it so that its a constant speed
-    }
+       rb.velocity = new Vector2(x,y).normalized * speed; //same as before, normalized makes it so that its a constant speed.
 
-    void changeSprite() {
-        if(spriteVersion == 0) {
-            spriteVersion = 1;
-        } else if(spriteVersion == 1) {
-            spriteVersion = 0;
-        }
-
-        sr.sprite = sprites[spriteVersion];
+        
     }
 }
